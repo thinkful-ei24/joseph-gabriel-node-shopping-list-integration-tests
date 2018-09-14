bd018efd21451691056b4637fbb6850eb9d64766
josephhess <readysteadyco@gmail.com>
2018-09-14 09:22:39 -0700

    add get and post tests for recipes

---
+++ test/test-recipes.js
@@ -0,0 +1,47 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { app, runServer, closeServer } = require('../server');
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('/recipes', function(){
+
+  before(function() { return runServer(); });
+  after(function() { return closeServer(); });
+ 
+  it('returns recipes on GET', function(){
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res){
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+
+        expect(res.body.length).to.be.at.least(1);
+
+        const expectedKeys = ['id', 'name','ingredients' ];
+        res.body.forEach(function(item){
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  it('creates a recipe on POST', function(){
+    const newRecipe = {name: 'chicken dinner', ingredients: ['chicken','dinner']};
+
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(function(res){
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'name', 'ingredients');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newRecipe, {id: res.body.id}));
+      });
+  });
+
+
+});
